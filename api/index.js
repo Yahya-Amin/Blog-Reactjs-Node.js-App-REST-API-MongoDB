ae4cc657adee220fb243abfc1d0141892d2a5e0a
Amin <amin.yahya@gmail.com>
2021-05-25 17:38:29 +0200

    user postman

--- api/index.js
+++ api/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
 
 dotenv.config();
-mongoose
-    .connect(process.env.MONGO_URL, {
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex:true,
@@ -14,7 +15,6 @@ mongoose
     .catch((err)=>console.log(err));
 
     app.use("/api/auth", authRoute);
-    
 app.listen("5000",()=>{
     console.log("Backend is runing");
 });
